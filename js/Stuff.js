25a48d3f0923871bf9c819bd515bc94a9819db84
Edward Thang <RandomClown@gmail.com>
2013-08-17 19:45:40 -0700

    Simple Canvas Draw

--- js/Stuff.js
+++ js/Stuff.js
@@ -4,4 +4,7 @@ window.onload=function() {
 	canvas=document.getElementById('canvas');
 	ctx=canvas.getContext('2d');
 
+	ballPic=document.getElementById('ball'); // Pulls the pic from the page
+
+	ctx.drawImage(ballPic, 8, 2); // Draws to the canvas
 }
