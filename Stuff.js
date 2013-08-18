a4cf2336bd5431c529f274a89875b89e2077c3e6
Edward Thang <RandomClown@gmail.com>
2013-08-17 19:37:17 -0700

    Preparing Canvas Code [Fix]
    I accidentally saved to the wrong location

--- Stuff.js
+++
@@ -1,7 +0,0 @@
-// JavaScript goes in this
-
-window.onload=function() {
-	canvas=document.getElementById('canvas');
-	ctx=canvas.getContext('2d');
-
-}
--- js/Stuff.js
+++ js/Stuff.js
@@ -1,2 +1,7 @@
 // JavaScript goes in this
 
+window.onload=function() {
+	canvas=document.getElementById('canvas');
+	ctx=canvas.getContext('2d');
+
+}
