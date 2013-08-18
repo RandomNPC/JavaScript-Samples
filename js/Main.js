dece0eeab8ec25a1d817e99e6cfb83be7e22578e
Edward Thang <RandomClown@gmail.com>
2013-08-18 04:54:25 -0700

    JS :: Onload & Canvas

--- js/Main.js
+++ js/Main.js
@@ -1,3 +1,12 @@
 //		Main Code
 
 
+var canvas, ctx;
+
+
+
+window.onload=function() { // Makes sure the website is loaded before running code
+	canvas=document.getElementById('canvas');
+	ctx=canvas.getContext('2d');
+
+};
