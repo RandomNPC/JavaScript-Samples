7c36b1da1406a3605442f3a13f8f9cc667962ea7
Edward Thang <RandomClown@gmail.com>
2013-08-19 03:55:01 -0700

    Added Sprite Animation Vars

--- js/Sprite.js
+++ js/Sprite.js
@@ -15,12 +15,26 @@ var Sprite=function(id) {
 	this.midX=this.sizeX/2; // Mid point of the sprite
 	this.midY=this.sizeY/2; // It will be centered here on canvas
 
+	/*
+		Some nice features to have in our animation:
+			   Loop = Restart the animation when it hits the end
+			Reverse = Play the animation backwards
+			  Pause = Pause the animation
+			   Idle = Only play the animation while in motion
+			           ^ Cars don't animate until moving, but people do
+	*/
+	this.hide=false;
+	this.loop=true;
+	this.reverse=false;
+	this.pause=false;
+	this.idle=false;
+
 
 	this.draw=function(ctx) {
 		if(ctx==undefined) throw ('Context not passed');
 		if(this.id==null) throw ('Sprite not initialized');
 
-		ctx.drawImage(
+		if(!this.hide) ctx.drawImage(
 			this.img,	// Image source
 			0, 0,    	// Offset in sprite sheet [which tile to draw]
 			this.sizeX, this.sizeY,	// Tile size
