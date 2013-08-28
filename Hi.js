44a5b69ff2272a06ecd0810468af9c5fa83a7d0c
Edward Thang <RandomClown@gmail.com>
2013-08-27 23:45:03 -0700

    Installation & Simple Output

---
+++ Hi.js
@@ -0,0 +1,15 @@
+/*
+	In this sample, I will be using a platform called Node.js.
+
+	To get it, you simply need to install Node.js from the website:
+		http://nodejs.org
+
+	Once it is installed, you can open up a terminal & cd to this directory.
+
+	In this directory, type:
+		node Hi.js
+
+	That will run this script from your terminal.  Magic, yes?
+*/
+
+console.log('Ohaider');
