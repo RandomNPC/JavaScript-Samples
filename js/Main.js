5059ad7081b9618935c114818392920b7d45eba0
Edward Thang <RandomClown@gmail.com>
2013-08-29 00:13:12 -0700

    Example: Side by Side Tanks

--- js/Main.js
+++ js/Main.js
@@ -14,10 +14,30 @@ window.onload=function() { // Makes sure the website is loaded before running co
 
 	tankBody=new Sprite('tankBody');
 	tankBody.setTileSize(128, 128);
+	tankBody._frameTimeMax=6;
 	tankBody.x=64;
 	tankBody.y=90;
 	tankBody.draw(ctx);
 
+	tankBodyB=new Sprite('tankBody');
+	tankBodyB.setTileSize(128, 128);
+	tankBodyB._frameTimeMax=3;
+	tankBodyB.x=128;
+	tankBodyB.y=90;
+	tankBodyB.draw(ctx);
+
+	tankBodyC=new Sprite('tankBody');
+	tankBodyC.setTileSize(128, 128);
+	tankBodyC._frameTimeMax=1;
+	tankBodyC.x=192;
+	tankBodyC.y=90;
+	tankBodyC.draw(ctx);
+
+	setInterval(function() {
+		tankBody.drawAni(ctx);
+		tankBodyB.drawAni(ctx);
+		tankBodyC.drawAni(ctx);
+	}, 42)
 
 
 	canvas.onmouseup=function() { // On click
